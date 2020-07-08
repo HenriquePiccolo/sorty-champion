a51e4d9906649939363b4d17b646cb77dfcae3e5
Ubuntu <ubuntu@ip-172-31-79-168.ec2.internal>
2020-07-08 01:54:40 +0000

    Apresentar quantidade de vezes que executou até chegar no resultado

--- sorty_champion.py
+++ sorty_champion.py
@@ -1,8 +1,10 @@
 import random
+import sys
 from prettytable import PrettyTable
 
-qtd_semanas = 7
-    
+## Apenas para controle de quantidade de execuções ate chegar no resultado
+x = 1
+
 champions = [
     "Henrique Piccolo",
     "Fernando Wiek",
@@ -18,18 +20,16 @@ n_buddies = [
     "Andre"
 ]
 
-def rulesChampion(champion, buddy, semana):
+def rulesChampion(champion, buddy, semana, semana_ver):
     ## Regra         
     ## Nao pode ser buddy quem esta na lista n_buddies
     ## Nao pode ser champion e buddy na mesma semana
     if buddy not in n_buddies and  champion != buddy:
-        ## Determina quantas semanas anteriores a verificar se a pessoa foi/nao champion/buddy
-        y = 1
-        while y >= 0:
+        while semana_ver >= 0:
             ## Verifica se foi champion ou buddy na semana y
-            if champion == champion_semana[semana-y] or champion == buddy_semana[semana-y] or buddy == champion_semana[semana-y] or buddy == buddy_semana[semana-y]:
+            if champion == champion_semana[semana-semana_ver] or champion == buddy_semana[semana-semana_ver] or buddy == champion_semana[semana-semana_ver] or buddy == buddy_semana[semana-semana_ver]:
                 return False
-            y -= 1
+            semana_ver -= 1
         return True
     return False
 
@@ -37,6 +37,18 @@ def main():
     global semana
     global champion_semana
     global buddy_semana
+    global qtd_semanas
+    global qtd_semanas_ver
+    global x
+    
+    ## Determina quantas semanas serao apresentadas de resultado
+    if len(sys.argv) > 1:
+        qtd_semanas = int(sys.argv[1])
+    else:
+        qtd_semanas = 10
+        
+    ## Determina quantas semanas anteriores a verificar se a pessoa foi/nao champion/buddy
+    qtd_semanas_ver = 1
     semana = 0
     
     champion_semana = [0] * qtd_semanas
@@ -56,7 +68,7 @@ def main():
             buddy = random.choice(champions)
             
             ## Valida regras de champion
-            if rulesChampion(champion, buddy, semana):
+            if rulesChampion(champion, buddy, semana, qtd_semanas_ver):
                 ## Adiciona valores na tabela para ser apresentado o resultado
                 t.add_row([semana, champion, buddy])
                 buddy_semana[semana] = buddy
@@ -75,9 +87,11 @@ def main():
                     value = True
                 semana += 1
             if value == False:
+                x += 1
                 main()
 
     print(t)
+    print("Foram processados " + str(x) + " vezes ate chegar neste resultado")
     exit()
 
 if __name__ == "__main__":
